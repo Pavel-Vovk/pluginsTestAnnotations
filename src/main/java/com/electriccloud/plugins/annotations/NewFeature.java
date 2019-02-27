ea7d3415308a60230fc1d14373c678341b018842
Pavel-vovk <wolf.od@gmail.com>
2019-02-27 02:23:48 +0200

    fix default

--- src/main/java/com/electriccloud/plugins/annotations/NewFeature.java
+++ src/main/java/com/electriccloud/plugins/annotations/NewFeature.java
@@ -17,5 +17,5 @@ import org.spockframework.runtime.extension.ExtensionAnnotation;
 @ExtensionAnnotation(NewFeatureExtension.class)
 public @interface NewFeature {
    //String value() default "";
-    String pluginVersion() default "[not defined]";
+    String pluginVersion() default "[unassigned]";
 }
